91e5d49ccbf4df756556863597ff89751dd167a7
Sondre Kongsgård <sondre.kongsgaard@gmail.com>
2017-07-08 14:09:23 +0200

    Rename function names to use snake case

--- led_controller/PixelLights.py
+++ led_controller/PixelLights.py
@@ -4,14 +4,14 @@
 import RPi.GPIO as GPIO, time, os, sys
 
 class PixelLights:
-    NUMBER_OF_PIXELS = 18 # set number of pixels in your strip
+    NUMBER_OF_PIXELS = 160 # Number of pixels in LED strip
     DEBUG = 1
     GPIO.setmode(GPIO.BCM)
-    SPICLK = 23 # The SPI clock pin on the raspberry pi, pin 23
-    SPIDO = 19 # The SPI data line (MOSI) on the raspberry pi, pin 19
-    ledpixels = [0] * NUMBER_OF_PIXELS
+    SPICLK = 23 # The SPI clock pin on the Raspberry Pi
+    SPIDO = 19 # The SPI data line (MOSI) on the Raspberry Pi
+    led_pixels = [0] * NUMBER_OF_PIXELS
 
-    def slowspiwrite(self, clockpin, datapin, byteout):
+    def slow_spi_write(self, clockpin, datapin, byteout):
         GPIO.setup(clockpin, GPIO.OUT)
         GPIO.setup(datapin, GPIO.OUT)
         for i in range(8):
@@ -23,7 +23,7 @@ class PixelLights:
             GPIO.output(clockpin, True)
             GPIO.output(clockpin, False)
 
-    def writestrip(self, pixels):
+    def write_strip(self, pixels):
         spidev = file("/dev/spidev0.0", "w")
         for i in range(len(pixels)):
             spidev.write(chr((pixels[i]>>16) & 0xFF))
@@ -32,62 +32,62 @@ class PixelLights:
         spidev.close()
         time.sleep(0.002)
 
-    def Color(self, r, g, b):
+    def color(self, r, g, b):
         return ((r & 0xFF) << 16) | ((g & 0xFF) << 8) | (b & 0xFF)
 
-    def setpixelcolor(self, pixels, n, r, g, b):
+    def set_pixel_color(self, pixels, n, r, g, b):
         if (n >= len(pixels)):
             return
-        pixels[n] = self.Color(r,g,b)
+        pixels[n] = self.color(r,g,b)
 
-    def setpixelcolor(self, pixels, n, c):
+    def set_pixel_color(self, pixels, n, c):
         if (n >= len(pixels)):
             return
         pixels[n] = c
 
-    def colorwipe(self, pixels, c, delay):
+    def color_wipe(self, pixels, c, delay):
         for i in range(len(pixels)):
-            self.setpixelcolor(pixels, i, c)
-            self.writestrip(pixels)
+            self.set_pixel_color(pixels, i, c)
+            self.write_strip(pixels)
             time.sleep(delay)
 
-    def Wheel(self, WheelPos):
-        if (WheelPos < 85):
-            return self.Color(WheelPos * 3, 255 - WheelPos * 3, 0)
-        elif (WheelPos < 170):
-            WheelPos -= 85;
-            return self.Color(255 - WheelPos * 3, 0, WheelPos * 3)
+    def wheel(self, wheel_pos):
+        if (wheel_pos < 85):
+            return self.color(wheel_pos * 3, 255 - wheel_pos * 3, 0)
+        elif (wheelPos < 170):
+            wheel_pos -= 85;
+            return self.Color(255 - wheel_pos * 3, 0, wheel_pos * 3)
         else:
-            WheelPos -= 170;
-            return self.Color(0, WheelPos * 3, 255 - WheelPos * 3)
+            wheel_pos -= 170;
+            return self.Color(0, wheel_pos * 3, 255 - wheel_pos * 3)
 
-    def rainbowCycle(self, pixels, wait):
+    def rainbow_cycle(self, pixels, wait):
         for color in range(256): # one cycle of all 256 colors in the wheel
             for pixel in range(len(pixels)):
                 # tricky math! we use each pixel as a fraction of the full 96-color wheel
                 # (thats the i / strip.numPixels() part)
                 # Then add in j which makes the colors go around per pixel
                 # the % 96 is to make the wheel cycle around
-                self.setpixelcolor(pixels, pixel, self.Wheel( ((pixel * 256 / len(pixels)) + color) % 256) )
-            self.writestrip(pixels)
+                self.set_pixel_color(pixels, pixel, self.wheel( ((pixel * 256 / len(pixels)) + color) % 256) )
+            self.write_strip(pixels)
             time.sleep(wait)
 
-    def cls(self, pixels):
+    def clear(self, pixels):
         for i in range(len(pixels)):
-            self.setpixelcolor(pixels, i, self.Color(0,0,0))
-            self.writestrip(pixels)
+            self.set_pixel_color(pixels, i, self.color(0,0,0))
+            self.write_strip(pixels)
 
     def main(self):
         try:
-            self.colorwipe(self.ledpixels, self.Color(255, 0, 0), 0.05)
-            self.colorwipe(self.ledpixels, self.Color(0, 255, 0), 0.05)
-            self.colorwipe(self.ledpixels, self.Color(0, 0, 255), 0.05)
-            self.rainbowCycle(self.ledpixels, 0.00)
-            self.cls(self.ledpixels)
+            self.color_wipe(self.led_pixels, self.color(255, 0, 0), 0.05)
+            self.color_wipe(self.led_pixels, self.color(0, 255, 0), 0.05)
+            self.color_wipe(self.led_pixels, self.color(0, 0, 255), 0.05)
+            self.rainbow_cycle(self.led_pixels, 0.00)
+            self.clear(self.led_pixels)
         except KeyboardInterrupt:
-            self.cls(self.ledpixels)
+            self.cls(self.led_pixels)
             sys.exit(0)
 
-if  __name__ =='__main__':
-    LEDs=PixelLights()
+if  __name__ == '__main__':
+    LEDs = PixelLights()
     LEDs.main()
--- led_controller/PixelLightsDemo.py
+++ led_controller/PixelLightsDemo.py
@@ -6,28 +6,28 @@ import time
 from PixelLights import PixelLights
 
 LEDs = PixelLights() # Set the name of our module
-NUMBER_OF_PIXELS = 18 # Set the number of pixels
-ledpixels = [0] * NUMBER_OF_PIXELS # set up the pixel array
+NUMBER_OF_PIXELS = 160 # Set the number of pixels
+led_pixels = [0] * NUMBER_OF_PIXELS # set up the pixel array
 
 try:
-    # LEDs.colorwipe(ledpixels, LEDs.Color(0, 0, 255), 0.05) # wipes blue pixels along the strip
+    # LEDs.color_wipe(led_pixels, LEDs.color(0, 0, 255), 0.05) # wipes blue pixels along the strip
 
-    LEDs.cls(ledpixels) # clears all the pixels to black
+    LEDs.clear(led_pixels) # clears all the pixels to black
     time.sleep(0.5)
 
-    # LEDs.rainbowCycle(ledpixels, 0.01) # rainbow swirl
+    # LEDs.rainbow_cycle(led_pixels, 0.01) # rainbow swirl
 
-    # LEDs.cls(ledpixels) # clears all the pixels to black
+    # LEDs.clear(led_pixels) # clears all the pixels to black
     # time.sleep(0.5)
 
     for i in range(0, 5):
-        LEDs.setpixelcolor(ledpixels, i, LEDs.Color(0,255,0)) # from ledpixels, sets pixel 10 (starting from 0) to green
-        LEDs.writestrip(ledpixels) # writes the pixels (must be called after setpixelcolor to update
+        LEDs.set_pixel_color(led_pixels, i, LEDs.color(0,255,0)) # from ledpixels, sets pixel 10 (starting from 0) to green
+        LEDs.write_strip(led_pixels) # writes the pixels (must be called after setpixelcolor to update
         time.sleep(0.5)
 
-    LEDs.cls(ledpixels) # clears all the pixels to black
+    LEDs.clear(led_pixels) # clears all the pixels to black
     time.sleep(0.5)
 
 except KeyboardInterrupt: # clears all pixels in the case of Ctrl-C exit
-    LEDs.cls(ledpixels)
+    LEDs.clear(led_pixels)
     sys.exit(0)
