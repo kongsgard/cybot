c73a77f16c1ba549fe120844ba2647ce4d216140
Sondre Kongsgård <sondre.kongsgaard@gmail.com>
2017-07-09 14:45:28 +0200

    servo_test.py: Add support for command line arguments

--- servo_controller/servo_test.py
+++ servo_controller/servo_test.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python
 import ServoController as sc
+import sys
 
 # Set all servo motors to home position
 with sc.ServoController() as servo:
     for i in range(0, 18):
-        servo.set_angle(i, 90)
+        servo.set_angle(i, int(sys.argv[1]))
